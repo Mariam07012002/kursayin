428e286b53f9676c70f22cb1f0652421cbb55c83
Mariam Grigoryan 07 <116749473+Mariam07012002@users.noreply.github.com>
2023-10-05 21:55:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const house_item = document.getElementById('house-item');
+const bank_item = document.getElementById('bank-item');
+
+const avand_varkanish_item = document.getElementById('avand-varkanish-item');
+const bank_varkanish_item = document.getElementById('bank-varkanish-item');
+const mermasin_item = document.getElementById('mermasin-item');
+const erashxiq_item = document.getElementById('erashxiq-item');
+const govazd_item = document.getElementById('govazd-item');
+
+house_item.addEventListener('click' ,()=>{
+   window.location.href = "index.html"
+})
+bank_item.addEventListener('click' ,()=>{
+    window.location.href = "bank.html"
+})
+avand_varkanish_item.addEventListener('click' ,()=>{
+    window.location.href = "avand.html"
+})
+bank_varkanish_item.addEventListener('click' ,()=>{
+    window.location.href = "bankvarkanish.html"
+})
+mermasin_item.addEventListener('click' ,()=>{
+    window.location.href = "mermasin.html"
+})
+erashxiq_item.addEventListener('click' ,()=>{
+    window.location.href = "erashxiq.html"
+})
+govazd_item.addEventListener('click' ,()=>{
+    window.location.href = "govazd.html"
+})
